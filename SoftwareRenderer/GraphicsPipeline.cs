beb5590399873bbd6f85d3223d957dc6a78f2d3e
Nick Conklin <nconkl1@outlook.com>
2017-09-29 11:12:31 -0400

    Add Vector.cs, add RasterizeTriangle function to GraphicsPipeline.cs

--- SoftwareRenderer/GraphicsPipeline.cs
+++ SoftwareRenderer/GraphicsPipeline.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,19 @@ namespace SoftwareRenderer
 {
 	class GraphicsPipeline
 	{
-		
+		public void RasterizeTriangle(Vector2 a, Vector2 b, Vector2 c, Bitmap targetImage)
+		{
+			var min = Vector2.Min(a, Vector2.Min(b, c));
+			var max = Vector2.Max(a, Vector2.Max(b, c));
+
+			Vector2 p;
+			for (p.y = min.y; p.y <= max.y; p.y += 1)
+			{
+				for (p.x = min.x; p.x <= max.x; p.x += 1)
+				{
+
+				}
+			}
+		}
 	}
 }
---
+++ SoftwareRenderer/Vector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareRenderer
+{
+	struct Vector2
+	{
+		public float x, y;
+
+		public static Vector2 Min(Vector2 a, Vector2 b)
+		{
+			return new Vector2 { x = Math.Min(a.x, b.x), y = Math.Min(a.y, b.y) };
+		}
+
+		public static Vector2 Max(Vector2 a, Vector2 b)
+		{
+			return new Vector2 { x = Math.Max(a.x, b.x), y = Math.Max(a.y, b.y) };
+		}
+
+	}
+}
